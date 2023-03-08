44da7158be32013fe21f3e39a689564038ed5271
omii623 <68omii68@gmail.com>
2023-03-08 23:34:03 +0100

    Az alap program létrehozása fejlesztéshez.

---
+++ jobon-app/src/main/java/hu/jobon/jobonapp/JobonAppApplication.java
@@ -0,0 +1,13 @@
+package hu.jobon.jobonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobonAppApplication.class, args);
+	}
+
+}
