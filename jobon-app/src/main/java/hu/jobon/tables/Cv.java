b049af448d5cbeb9d7a4719d8364b01554729056
omii623 <68omii68@gmail.com>
2023-03-17 20:50:21 +0100

    Adatbázis táblák létrehozása.

---
+++ jobon-app/src/main/java/hu/jobon/tables/Cv.java
@@ -0,0 +1,27 @@
+package hu.jobon.tables;
+
+public class Cv {
+    private int felhasznalo_ID;
+    private String oneletrajz;
+    
+    public Cv(int felhasznalo_ID, String oneletrajz) {
+        this.felhasznalo_ID = felhasznalo_ID;
+        this.oneletrajz = oneletrajz;
+    }
+
+    public int getFelhasznalo_ID() {
+        return felhasznalo_ID;
+    }
+
+    public void setFelhasznalo_ID(int felhasznalo_ID) {
+        this.felhasznalo_ID = felhasznalo_ID;
+    }
+
+    public String getOneletrajz() {
+        return oneletrajz;
+    }
+
+    public void setOneletrajz(String oneletrajz) {
+        this.oneletrajz = oneletrajz;
+    }
+}
---
+++ jobon-app/src/main/java/hu/jobon/tables/Szakma.java
@@ -0,0 +1,27 @@
+package hu.jobon.tables;
+
+public class Szakma {
+    private String szakma;
+    private int felhasznalo_ID;
+    
+    public Szakma(String szakma, int felhasznalo_ID) {
+        this.szakma = szakma;
+        this.felhasznalo_ID = felhasznalo_ID;
+    }
+
+    public String getSzakma() {
+        return szakma;
+    }
+
+    public void setSzakma(String szakma) {
+        this.szakma = szakma;
+    }
+
+    public int getFelhasznalo_ID() {
+        return felhasznalo_ID;
+    }
+
+    public void setFelhasznalo_ID(int felhasznalo_ID) {
+        this.felhasznalo_ID = felhasznalo_ID;
+    }
+}
