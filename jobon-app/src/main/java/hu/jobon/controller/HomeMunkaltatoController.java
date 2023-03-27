2c4f39472bd152a89695e261a425da0ae3a636e8
Molnar David <h161706@stud.u-szeged.hu>
2023-03-28 00:25:38 +0200

    tablak listazasa, minden tipusu felhasznalohoz

---
+++ jobon-app/src/main/java/hu/jobon/controller/HomeMunkaltatoController.java
@@ -0,0 +1,72 @@
+package hu.jobon.controller;
+
+import hu.jobon.database.Database;
+import hu.jobon.database.model.Allasajanlat;
+import hu.jobon.database.model.Allaskereso;
+import hu.jobon.database.model.Munkaltato;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
+
+import static hu.jobon.controller.LoginController.felhasznalo;
+
+public class HomeAllaskeresoController {
+    @FXML
+    public TableView tv1;
+    @FXML
+    public TextField email;
+    @FXML
+    public TextField pass;
+
+    Database db = new Database();
+    @FXML
+    public void initialize(){
+        TableColumn munkaltatoCol = new TableColumn("felhasznalo_ID");
+        munkaltatoCol.setCellValueFactory(new PropertyValueFactory<>("felhasznalo_ID"));
+        TableColumn oraberCol = new TableColumn("oraber");
+        oraberCol.setCellValueFactory(new PropertyValueFactory<>("oraber"));
+        TableColumn pozicioCol = new TableColumn("pozicio");
+        pozicioCol.setCellValueFactory(new PropertyValueFactory<>("pozicio"));
+        TableColumn munkakorCol = new TableColumn("munkakor");
+        munkakorCol.setCellValueFactory(new PropertyValueFactory<>("munkakor"));
+        TableColumn leirasCol = new TableColumn("leiras");
+        leirasCol.setCellValueFactory(new PropertyValueFactory<>("leiras"));
+        TableColumn letrehozasCol = new TableColumn("letrehozas_ideje");
+        letrehozasCol.setCellValueFactory(new PropertyValueFactory<>("letrehozas_ideje"));
+        tv1.getColumns().addAll(munkaltatoCol, oraberCol, pozicioCol, munkakorCol, leirasCol, letrehozasCol);
+ }
+
+    @FXML
+    void listazz() {
+        List<Allasajanlat> allasok = db.getAllasajanlatAll();
+        tv1.getItems().clear();
+        for (Allasajanlat allas : allasok) {
+            System.out.println(allas.getMunkakor());
+            tv1.getItems().add(allas);
+        }
+    }
+
+    public void adataim(ActionEvent actionEvent) {
+        email.setText(felhasznalo.getEmail_cim());
+        pass.setText(felhasznalo.getJelszo());
+    }
+
+    private String UPDATE_ADMIN;
+
+    public void modositas(ActionEvent actionEvent) {
+        felhasznalo.setEmail_cim(email.getText());
+        felhasznalo.setJelszo(pass.getText());
+        UPDATE_ADMIN = "UPDATE C##SAELDC.FELHASZNALO SET EMAIL_CIM = '" + felhasznalo.getEmail_cim() + "', JELSZO ='" + felhasznalo.getJelszo() + "' WHERE ID = " + Integer.toString(felhasznalo.getID()) ;
+        System.out.println(felhasznalo.getEmail_cim());
+        System.out.println(felhasznalo.getJelszo());
+        System.out.println(UPDATE_ADMIN);
+        db.updateFelhasznalo(UPDATE_ADMIN);
+    }
+
+
+}
